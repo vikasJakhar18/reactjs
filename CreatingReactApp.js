47dc9dc81242c9c3a759823cb90470967cffb2db
Vikas Choudhary <vikasjakhar93262@gmail.com>
2025-02-01 23:27:30 +0530

    CreatingReactApp.js

---
+++ CreatingReactApp.js
@@ -0,0 +1,18 @@
+//App.js
+
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Hello , My name is Vikas Choudhary</p>
+    </>
+  )
+}
+
+export default App
+
