45468cabe65094a42209d3f9d7ccaec433b444fc
Vikas Choudhary <vikasjakhar93262@gmail.com>
2025-02-03 14:51:12 +0530

    Update UseStateHook.js

--- UseStateHook.js
+++ UseStateHook.js
@@ -15,3 +15,20 @@ function App() {
 export default App
 
 //Components.....................................................
+//Counter.jsx
+import React, { useState } from 'react'
+import './Counter.css'
+
+export default function counter() {
+
+ const [count,setCount]=useState(0)
+  return (
+    <div className='counter'>
+      <p id='para'> you have clicked {count} times</p>
+      <button id='btn' onClick={()=>{setCount(count+1)}}> click me!</button>
+    </div>
+  )
+}
+
+//Counter.css
+
